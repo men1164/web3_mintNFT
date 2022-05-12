8eacf3c4b4444546a1b666e01b4721f1f521f897
men1164 <41180013+men1164@users.noreply.github.com>
2022-05-12 19:30:42 +0700

    user can minting NFTs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,10 @@ import WalletBalance from "./WalletBalance";
 import { ethers } from "ethers";
 import MyNFT from '../artifacts/contracts/MyNFT.sol/MyNFT.json';
 import { useEffect, useState } from "react";
+import NFTImage from "./NFTImage";
 
 // Replace deployed contract's address here
-const contractAddress = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 
@@ -32,7 +33,7 @@ export default function Home() {
       <WalletBalance />
       {Array(totalMinted + 1).fill(0).map((_, i) => (
         <div key={i}>
-          <p>NFT {i}</p>
+          <NFTImage tokenId={i} getCount={getCount} />
         </div>
       ))}
     </div>
---
+++ src/components/NFTImage.jsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import MyNFT from '../artifacts/contracts/MyNFT.sol/MyNFT.json';
+
+// Replace deployed contract's address here
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+// get the end user
+const signer = provider.getSigner();
+
+// get the smart contract
+const contract = new ethers.Contract(contractAddress, MyNFT.abi, signer);
+
+export default function NFTImage({ tokenId, getCount }) {
+  // replace pinata content's id here
+  const contentId = "Qmajhpj5iq3PD1yRnQQJ5D94mYY4JDunx4Tge4jR4UJTja";
+
+  const metaDataURI = `${contentId}/${tokenId}.json`;
+  // const imageURI = `https://gateway.pinata.cloud/ipfs/${contentId}/${tokenId}.png`;
+  const imageURI = `src/img/${tokenId}.png`;
+  const [isMinted, setIsMinted] = useState(false);
+
+  const getMintedStatus = async () => {
+    const result = await contract.isContentOwned(metaDataURI);
+    console.log(result)
+    setIsMinted(result);
+  };
+
+  const mintToken = async () => {
+    const connection = contract.connect(signer);
+    const addr = connection.address;
+    const result = await contract.payToMint(addr, metaDataURI, {
+      value: ethers.utils.parseEther('0.05'),
+    });
+
+    await result.wait();
+    getMintedStatus();
+    getCount();
+  };
+  
+  async function getURI() {
+    const uri = await contract.tokenURI(tokenId);
+    alert(uri);
+  }
+
+  useEffect(() => {
+    getMintedStatus();
+  }, [isMinted]);
+
+  return (
+    <div>
+      <img src={isMinted ? imageURI : ''} style={{ width: '160px' }} />
+        <h5>ID #{tokenId}</h5>
+        {!isMinted ? (
+          <button onClick={mintToken}>
+            Mint
+          </button>
+        ) : (
+          <button onClick={getURI}>
+            Taken! Show URI
+          </button>
+        )}
+    </div>
+  );
+}
