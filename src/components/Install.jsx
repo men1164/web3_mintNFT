704d945e593f4d842f15ba6b1733b47dd460e243
men1164 <41180013+men1164@users.noreply.github.com>
2022-05-12 16:24:22 +0700

    created Install and Home component, nested in App.jsx

---
+++ src/components/Install.jsx
@@ -0,0 +1,8 @@
+export default function Install() {
+  return (
+    <div>
+      <h3>Follow the link to install MetaMask!</h3>
+      <a href="https://metamask.io/download.html">Download</a>
+    </div>
+  )
+}
