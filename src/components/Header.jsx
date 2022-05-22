c4d8643789504ac8e57bbf55b90c526263be24c1
men1164 <41180013+men1164@users.noreply.github.com>
2022-05-22 15:26:59 +0700

    mint form style added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <div className="w-full text-center mt-10">
       <h3 className="font-medium text-6xl text-white">
-        Mint Your <span className="font-bold bg-clip-text text-transparent  bg-gradient-to-r from-[#30CFD0] to-[#c43ad6]">NFT</span> Today!
+        Mint Your <span className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">NFT</span> Today!
       </h3>
     </div>
   )
--- src/components/MintForm.jsx
+++ src/components/MintForm.jsx
@@ -18,31 +18,35 @@ export default function MintForm({ getCount }) {
   const [inputFile, setFile] = useState(null);
   const [nftName, setNftName] = useState('');
   const [description, setDescription] = useState('');
+  const [isMinting, setIsMinting] = useState(false);
+  const [error, setIsError] = useState(null);
+  const [fileError, setFileError] = useState(null);
 
 
   const types = ["image/jpeg", "image/png"];
   const url = "https://api.pinata.cloud/pinning/pinFileToIPFS";
   const url2 = `https://api.pinata.cloud/pinning/hashMetadata`;
-  let uri;
+  let cid;
 
   const fileChange = e => {
     setFile(null)
     let selected = e.target.files[0]
 
     if(selected && types.includes(selected.type)) {
-      setFile(selected)
+      setFile(selected);
+      setFileError(null);
     }
     else {
       console.log("fail")
       setFile(null)
-      // setThumbnailError('Please selected an image file (PNG or JPG)')
+      setFileError('Please selected an image file (PNG or JPG)')
     }
   }
 
   const mintToken = async () => {
     const connection = contract.connect(signer);
     const addr = connection.address;
-    const result = await contract.payToMint(addr, uri, {
+    const result = await contract.payToMint(addr, cid, {
       value: ethers.utils.parseEther('0.05'),
     });
 
@@ -50,8 +54,16 @@ export default function MintForm({ getCount }) {
     getCount();
   };
 
+  const clearForm = () => {
+    setNftName('');
+    setDescription('');
+    setFile(null);
+  }
+
   const handleSubmit = async e => {
     e.preventDefault();
+    setIsMinting(false);
+    setIsError(null);
 
     const metaData = { name: nftName, keyvalues: { description } };
 
@@ -60,6 +72,7 @@ export default function MintForm({ getCount }) {
     formData.append("pinataMetadata", JSON.stringify(metaData));
 
     try {
+      setIsMinting(true);
       const res = await axios.post(url, formData, {
         headers: {
           'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
@@ -68,7 +81,7 @@ export default function MintForm({ getCount }) {
         }
       });
       console.log(res)
-      uri = res.data.IpfsHash;
+      cid = res.data.IpfsHash;
       
       // MetaData for NFTs update
       const metaData2 = { ipfsPinHash: res.data.IpfsHash, ...metaData, keyvalues: { description, image: `ipfs://${res.data.IpfsHash}` }};
@@ -82,26 +95,52 @@ export default function MintForm({ getCount }) {
       console.log(res2);
       
       mintToken();
+      setIsMinting(false);
+      clearForm();
     }
     catch(err) {
+      setIsMinting(false);
       console.log(err.message);
+      setIsError(err.message);
     }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        <li>
-          <label>Name: </label>
-          <input type="text" onChange={e => setNftName(e.target.value)} />
-        </li>
-        <li>
-          <label>Description: </label>
-          <input type="text" onChange={e => setDescription(e.target.value)} />
-        </li>
-        <input type="file" onChange={fileChange} />
-      </ul>
-      <button type="submit">Submit</button>
-    </form>
+    <div className="p-5 mt-10 mx-auto h-full w-2/5 bg-gray-200 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-20 border border-gray-100 border-opacity-40">
+      <form className="text-white" onSubmit={handleSubmit}>
+        <label className="block">
+          <span className="block mb-1">Name: </span>
+          <input className="shadow border border-opacity-20 border-white bg-gray-600 text-gray-400 appearance-none rounded w-full py-1 px-2 leading-tight focus:outline-none focus:shadow-outline" type="text" onChange={e => setNftName(e.target.value)} value={nftName} required />
+        </label>
+        <label className="block mt-2">
+          <span className="block mb-1">Description: </span>
+          <textarea className="shadow border border-opacity-20 border-white bg-gray-600 text-gray-400 appearance-none rounded w-full py-1 px-2 leading-tight focus:outline-none focus:shadow-outline" type="text" onChange={e => setDescription(e.target.value)} rows="3" value={description} required />
+        </label>
+        <label className="block mt-2">
+          <span className="block mb-1">File: </span>
+          <input className="file:mr-4 file:py-2 file:px-4
+                            file:rounded-full file:border-0
+                            file:text-sm file:bg-violet-50 file:text-violet-700
+                            hover:file:bg-violet-100 hover:file:cursor-pointer hover:cursor-pointer" 
+          type="file" onChange={fileChange} required />
+        </label>
+        {error &&
+          <p className="mt-2 text-sm text-red-500 text-center">{error}</p>
+        }
+        {fileError &&
+          <p className="mt-2 text-sm text-red-500 text-center">{fileError}</p>
+        }
+        {!isMinting && 
+          <button className="block mt-5 bg-indigo-500 hover:bg-indigo-600 shadow-lg hover:shadow-indigo-400/50 shadow-indigo-500/50 rounded-full w-1/2 mx-auto px-3 py-2" type="submit">
+            Mint
+          </button>
+        }
+        {isMinting && 
+          <button className="block mt-5 bg-indigo-500 hover:bg-indigo-600 shadow-lg hover:shadow-indigo-400/50 shadow-indigo-500/50 rounded-full w-1/2 mx-auto px-3 py-2" type="submit" disabled>
+            Processing...
+          </button>
+        }
+      </form>
+    </div>
   )
 }
